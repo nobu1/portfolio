7eff35bf624cdbc68c660aa68eb5719e05774a69
Shinji Nobuhara <55390492+nobu1@users.noreply.github.com>
2023-06-29 21:32:10 +0900

    Add files via upload

--- Algorithms/DivisorCheck.cpp
+++ Algorithms/DivisorCheck.cpp
@@ -1,1 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+  int A, B;
+  bool checkDivosr = false;
+  cin >> A >> B;
+  
+  for(int i = A; i <= B; i++) {
+    if (100 % i == 0) {
+      checkDivosr = true;
+      break;
+    }
+  }
+  
+  if (checkDivosr) {
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
+
+  return 0;
+}
