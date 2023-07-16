55f928bff43b19b3e2565288626a8dc5ac7d01c9
Shinji Nobuhara <55390492+nobu1@users.noreply.github.com>
2023-07-16 12:28:15 +0900

    Add methods related pagination.

--- BlogSite/src/main/java/dao/ArticlesDAO.java
+++ BlogSite/src/main/java/dao/ArticlesDAO.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.Map;
 
 import model.ArticleData;
@@ -38,13 +39,38 @@ public class ArticlesDAO {
 		Connection con = null;
 		String sql = "SELECT article_title, file_path FROM blog_articles.articles WHERE article_user = '"
 				+ nickName + "'" + " AND delete_flag = 'false'";
-		
+
 		try {
 			con = condis.getConnection();
 			PreparedStatement psmt = con.prepareStatement(sql);
 			ResultSet rs = psmt.executeQuery();
 			while (rs.next()) {
-				articles.put(rs.getString("article_title"), rs.getString("file_path")) ;
+				articles.put(rs.getString("article_title"), rs.getString("file_path"));
+			}
+		} catch (SQLException e) {
+			System.out.println(e);
+			con.rollback();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			condis.close(con);
+		}
+		return articles;
+	}
+
+	public Map<String, String> getArticles(int offset, int recordPerPage) throws SQLException {
+		Map<String, String> articles = new HashMap<>();
+		ConnectDisconnectDAO_Articles condis = new ConnectDisconnectDAO_Articles();
+		Connection con = null;
+		String sql = "SELECT article_title, file_path FROM blog_articles.articles WHERE delete_flag = 'false' ORDER BY article_title LIMIT "
+				+ offset + ", " + recordPerPage;
+
+		try {
+			con = condis.getConnection();
+			PreparedStatement psmt = con.prepareStatement(sql);
+			ResultSet rs = psmt.executeQuery();
+			while (rs.next()) {
+				articles.put(rs.getString("article_title"), rs.getString("file_path"));
 			}
 		} catch (SQLException e) {
 			System.out.println(e);
@@ -57,17 +83,19 @@ public class ArticlesDAO {
 		return articles;
 	}
 	
-	public Map<String, String> getAllArticleList(Map<String, String> articles) throws SQLException {
+	public Map<String, String> getArticlesByNickName(int offset, int recordPerPage, String nickName) throws SQLException {
+		Map<String, String> articles = new HashMap<>();
 		ConnectDisconnectDAO_Articles condis = new ConnectDisconnectDAO_Articles();
 		Connection con = null;
-		String sql = "SELECT article_title, file_path FROM blog_articles.articles WHERE delete_flag = 'false'";
-		
+		String sql = "SELECT article_title, file_path FROM blog_articles.articles WHERE delete_flag = 'false' AND article_user = '" +
+						 nickName + "' ORDER BY article_title LIMIT " + offset + ", " + recordPerPage;
+
 		try {
 			con = condis.getConnection();
 			PreparedStatement psmt = con.prepareStatement(sql);
 			ResultSet rs = psmt.executeQuery();
 			while (rs.next()) {
-				articles.put(rs.getString("article_title"), rs.getString("file_path")) ;
+				articles.put(rs.getString("article_title"), rs.getString("file_path"));
 			}
 		} catch (SQLException e) {
 			System.out.println(e);
@@ -78,6 +106,53 @@ public class ArticlesDAO {
 			condis.close(con);
 		}
 		return articles;
+	}
+
+	public int getArticleCount() throws SQLException {
+		int count = 0;
+		ConnectDisconnectDAO_Articles condis = new ConnectDisconnectDAO_Articles();
+		Connection con = null;
+		String sql = "SELECT COUNT(*) cnt FROM blog_articles.articles WHERE delete_flag = 'false'";
+		
+		try {
+			con = condis.getConnection();
+			PreparedStatement psmt = con.prepareStatement(sql);
+			ResultSet rs = psmt.executeQuery();
+			rs.next();
+			count = rs.getInt("cnt");
+		} catch (SQLException e) {
+			System.out.println(e);
+			con.rollback();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			condis.close(con);
+		}
+		return count;
+	}
+	
+	public int getArticleCountByNickName(String nickName) throws SQLException {
+		int count = 0;
+		ConnectDisconnectDAO_Articles condis = new ConnectDisconnectDAO_Articles();
+		Connection con = null;
+		String sql = "SELECT COUNT(*) cnt FROM blog_articles.articles WHERE delete_flag = 'false' AND article_user = '"	+ 
+						 nickName + "'";
+		
+		try {
+			con = condis.getConnection();
+			PreparedStatement psmt = con.prepareStatement(sql);
+			ResultSet rs = psmt.executeQuery();
+			rs.next();
+			count = rs.getInt("cnt");
+		} catch (SQLException e) {
+			System.out.println(e);
+			con.rollback();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			condis.close(con);
+		}
+		return count;
 	}
 	
 	public Boolean checkTitle(String nickName, String title) throws SQLException {
@@ -86,7 +161,7 @@ public class ArticlesDAO {
 		Connection con = null;
 		String sql = "SELECT article_title FROM blog_articles.articles WHERE article_user = '"
 				+ nickName + "'" + " AND delete_flag = 'false'" + " AND article_title = '" + title + "'";
-		
+
 		try {
 			con = condis.getConnection();
 			PreparedStatement psmt = con.prepareStatement(sql);
@@ -104,13 +179,13 @@ public class ArticlesDAO {
 		}
 		return checkFlag;
 	}
-	
+
 	public void deleteArticle(String nickName, String title) throws SQLException {
 		ConnectDisconnectDAO_Articles condis = new ConnectDisconnectDAO_Articles();
 		Connection con = null;
-		String sql = "UPDATE blog_articles.articles SET delete_flag = '1' WHERE article_user = '" 
-							+ nickName + "' " + "AND article_title = '" +  title + "'";
-		
+		String sql = "UPDATE blog_articles.articles SET delete_flag = '1' WHERE article_user = '"
+				+ nickName + "' " + "AND article_title = '" + title + "'";
+
 		try {
 			con = condis.getConnection();
 			PreparedStatement psmt = con.prepareStatement(sql);
@@ -124,13 +199,13 @@ public class ArticlesDAO {
 			condis.close(con);
 		}
 	}
-	
+
 	public void updateEditArticleTitle(String nickName, String title, String articleURL) throws SQLException {
 		ConnectDisconnectDAO_Articles condis = new ConnectDisconnectDAO_Articles();
 		Connection con = null;
-		String sql = "UPDATE blog_articles.articles SET article_title ='" + title + "' WHERE article_user = '" 
-				+ nickName + "' " + "AND file_path = '" + articleURL +"'";
-		
+		String sql = "UPDATE blog_articles.articles SET article_title ='" + title + "' WHERE article_user = '"
+				+ nickName + "' " + "AND file_path = '" + articleURL + "'";
+
 		try {
 			con = condis.getConnection();
 			PreparedStatement psmt = con.prepareStatement(sql);
